135e939c0a1b8fd60b72bac59243ddf0ba9fa911
jieyin6 <961145934@qq.com>
2018-01-17 00:58:35 +0800

    add_function

--- vue_practice/js/03js.js
+++ vue_practice/js/03js.js
@@ -6,29 +6,35 @@ var app =new Vue({
 			id:1,
 			name:"iphone7",
 			price:6188,
-			count:1
+			count:1,
+			checkboxx:true
 		},
 		{
 			id:2,
 			name:"ipad pro",
 			price:5888,
-			count:1
+			count:1,
+			checkboxx:true
 		},
 		{
 			id:3,
-		
-		name:"macbook pro",
+			name:"macbook pro",
 			price:21488,
-			count:1
+			count:1,
+			checkboxx:true
 		}
-		]
+		],
+		checked:true
+		
 	},
 	computed:{
 		totalPrice:function(){
 			var total = 0;
 			for (var i = 0 ; i < this.list.length ; i++) {
+				if(this.list[i].checkboxx){
 				var item = this.list[i];
 				total += item.price * item.count;
+				}
 			}
 			return total.toString().replace(/\B(?=(\d{3})+$)/g,",");
 		}
@@ -43,7 +49,30 @@ var app =new Vue({
 		},
 		handleRemove:function(index){
 			this.list.splice(index,1);
+		},
+		removePick:function(){
+			this.picked=false;
+		},
+		bothPick:function(){
+			console.log(this.checked);
+			if(this.checked == true){
+				for(var i = 0 ;i < this.list.length;i++){
+					this.list[i].checkboxx=false;
+				}
+				
+			}else{
+				 for(var j = 0 ;j < this.list.length;j++){
+					this.list[j].checkboxx=true;
+					
+				}
+			}
+		},
+		delBothPick:function(){
+			if(this.checked == true){
+				this.checked = false;
+			}
 		}
+		
 	}
 	
 });
